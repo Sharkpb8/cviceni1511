cfc3769286664f41872d1bacc7300171f97f9bb0
sharkpb8 <sharkpb8@gmail.com>
2023-11-15 08:42:22 +0100

    some functions

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 let b = "ahoj";
 console.log(b);
+var a = 4;
+var c  = 3;
+var nasobek = a*c;
+console.log(nasobek);
+var delitel = a/c;
+console.log(delitel);
+var plus = a+c;
+console.log(plus);
+var minus = a-c;
+console.log(minus);
+var sin =   Math.sin(a)
+console.log(sin);
+var cos = Math.cos(a);
